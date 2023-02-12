8918b1436328124505634610365c0700f0e8a039
Max Nikitenko <max.nikitenko@namecheap.com>
2023-02-12 15:28:15 +0200

    feat(auth): Add identity provider;

--- examples/vanilla_client.py
+++ examples/vanilla_client.py
@@ -1,8 +1,12 @@
+import os
 
+from margay.sdk.auth import JWTAuth
 from margay.sdk.client import Client
-
 
 if __name__ == '__main__':
     token = input("Auth token:")
+    if not token:
+        app_secret = os.getenv("MARGAY_AUTH_SECRET", "SuperSecret")
+        token = JWTAuth(app_secret).issue_token("JohnSnow")
     client = Client("ws://127.0.0.1:8080", token)
     client.run()
---
+++ margay/sdk/auth/__init__.py
@@ -0,0 +1,25 @@
+import socket
+from datetime import timedelta, datetime, timezone
+
+import jwt
+
+
+class JWTAuth:
+    encoder = jwt.encode
+
+    def __init__(self, secret: str):
+        self.secret = secret
+
+    def issue_token(self, client: str) -> str:
+        return self.encoder(self.get_claims(client), self.secret, algorithm="HS256")
+
+    @staticmethod
+    def get_claims(client: str, ttl_hours=24) -> dict:
+        return {
+            "Audience": "localhost",
+            "ExpiresAt": datetime.now(tz=timezone.utc) + timedelta(hours=ttl_hours),
+            "Id": client,
+            "IssuedAt": datetime.now(tz=timezone.utc),
+            "Issuer": f"MargayPythonSDK-{socket.gethostname()}",
+            "Subject": "client",
+        }
